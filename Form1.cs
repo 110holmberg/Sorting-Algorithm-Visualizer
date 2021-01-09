d45c28fe3a9601c8ebe928b402d08680b426d364
110holmberg <76858506+110holmberg@users.noreply.github.com>
2021-01-09 02:53:22 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortingAlgorithmVisualizer
+{
+    public partial class Form1 : Form
+    {
+        int[] theArray;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            int numEntries = panel1.Width;
+            int maxVal = panel1.Height;
+            theArray = new int[numEntries];
+            g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.Black), 0, 0, numEntries, maxVal);
+            Random rand = new Random();
+            for (int i = 0; i < numEntries; i++)
+            {
+                theArray[i] = rand.Next(0, maxVal);
+            }
+            for (int i = 0; i < numEntries; i++)
+            {
+                g.FillRectangle(new System.Drawing.SolidBrush(System.Drawing.Color.White), i, maxVal - theArray[i], 1, maxVal);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            iSortEngine se = new bubbleSortEngine();
+            se.DoWork(theArray, g, panel1.Height);
+        }
+    }
+}
---
+++ bubbleSortEngine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortingAlgorithmVisualizer
+{
+    class bubbleSortEngine : iSortEngine
+    {
+        private bool sorted = false;
+        private int[] theArray;
+        private Graphics g;
+        private int maxVal;
+        Brush whiteBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+        Brush blackBrush =  new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+        public void DoWork(int[] theArrayIn, Graphics gIn, int maxValIn)
+        {
+            theArray = theArrayIn;
+            g = gIn;
+            maxVal = maxValIn;
+            while (!sorted)
+            {
+                for (int i = 0; i < theArray.Count() - 1; i++)
+                {
+                    if (theArray[i] > theArray[i + 1])
+                    {
+                        swap(i, i + 1);
+                    }
+
+                }
+                sorted = isSorted();
+            }
+        }
+        private bool isSorted()
+        {
+            for (int i = 0; i < theArray.Count() - 1; i++)
+            {
+                if (theArray[i] > theArray[i + 1]) return false;
+            }
+            return true;
+        }
+        private void swap(int i, int v)
+        {
+            int temp = theArray[i];
+            theArray[i] = theArray[i + 1];
+            theArray[i + 1] = temp;
+            g.FillRectangle(blackBrush, i, 0, 1, maxVal);
+            g.FillRectangle(blackBrush, v, 0, 1, maxVal);
+            g.FillRectangle(whiteBrush, i, maxVal - theArray[i], 1, maxVal);
+            g.FillRectangle(whiteBrush, v, maxVal - theArray[v], 1, maxVal);
+
+
+        }
+    }
+}
